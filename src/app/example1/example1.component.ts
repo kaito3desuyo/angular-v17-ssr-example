65ff27b792f3b5a87948b5cbf4116e8471d550c9
kaito3desuyo <admin@seapolis.me>
2023-12-17 11:47:01 +0900

    feat: :sparkles: Angular Material コンポーネントを追加する

--- src/app/example1/example1.component.ts
+++ src/app/example1/example1.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select'
 
 @Component({
   selector: 'app-example1',
   standalone: true,
   imports: [
     CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatSelectModule
   ],
   templateUrl: './example1.component.html',
   styleUrl: './example1.component.scss',
